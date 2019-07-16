f5e599ae13a277c7802b20c901d61c9e9703453c
henriknelson <henrik@cliffords.nu>
2019-07-16 10:21:38 +0200

    Fixed bash module

--- modules/bash/build.sh
+++ modules/bash/build.sh
@@ -1,7 +1,7 @@
 MAGISK_MODULE_HOMEPAGE=https://www.gnu.org/software/bash/
 MAGISK_MODULE_DESCRIPTION="A sh-compatible shell that incorporates useful features from the Korn shell (ksh) and C shell (csh)"
 MAGISK_MODULE_LICENSE="GPL-3.0"
-#MAGISK_MODULE_DEPENDS="ncurses, readline (>= 8.0), libandroid-support, termux-tools, command-not-found"
+MAGISK_MODULE_DEPENDS="ncurses, readline (>= 8.0), libandroid-support"
 _MAIN_VERSION=5.0
 _PATCH_VERSION=7
 MAGISK_MODULE_SHA256=b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d
@@ -27,14 +27,14 @@ MAGISK_MODULE_EXTRA_CONFIGURE_ARGS+=" bash_cv_getcwd_malloc=yes"
 
 MAGISK_MODULE_CONFFILES="etc/bash.bashrc etc/profile"
 
-MAGISK_MODULE_RM_AFTER_INSTALL="share/man/man1/bashbug.1 bin/bashbug"
+MAGISK_MODULE_RM_AFTER_INSTALL="share/* bin/bashbug"
 
 magisk_step_configure() {
-        cd $MAGISK_MODULE_SRCDIR
-	mkdir $MAGISK_MODULE_MASSAGEDIR/bin
 	target_host=aarch64-linux-gnu
-	./configure \
+	$MAGISK_MODULE_SRCDIR/configure \
 		--host=$target_host \
+		--prefix=$MAGISK_PREFIX \
+		--datarootdir=$MAGISK_MODULE_TMPDIR \
 		--disable-nls \
 		--enable-static-link  \
 		--without-bash-malloc \
@@ -47,32 +47,31 @@ magisk_step_configure() {
 		--enable-multibyte \
 		--enable-job-control \
 		--enable-array-variables \
-		CFLAGS="-g -O2 -static" \
-		LDFLAGS="-g -O2 -static"
-
-	make -j $(nproc)
-	cp $MAGISK_MODULE_SRCDIR/bash $MAGISK_MODULE_MASSAGEDIR/bin/bash
+		CFLAGS="-g -O2" \
+		LDFLAGS="-g -O2"
 }
 
-magisk_step_patch_module() {
-	PVER=$(echo $MAGISK_MODULE_VERSION | sed 's/\.//')
-	PVER="50"
-	cd $MAGISK_MODULE_SRCDIR
-  	echo "Applying patches"
 
-	for i in {001..007}; do
-		echo $i
-    		wget http://mirrors.kernel.org/gnu/bash/bash-$_MAIN_VERSION-patches/bash$PVER-$i 2>/dev/null
-    		if [ -f "bash$PVER-$i" ]; then
+magisk_step_patch_module() {
+	PVER=$(echo $_MAIN_VERSION | sed 's/\.//')
+
+  	echo "Applying online patches.."
+	for i in {001..015}; do
+    		if wget -q "http://mirrors.kernel.org/gnu/bash/bash-$_MAIN_VERSION-patches/bash$PVER-$i" > /dev/null; then
+			echo "Patching bash$PVER-$i"
 			patch -p0 -i bash$PVER-$i
 			rm -f bash$PVER-$i
+		else
+			continue
 		fi
 	done
 
-	echo "$MAGISK_MODULE_BUILDER_DIR"
+	echo "\n\n"
+
+	echo "Applying local patches.."
 	for i in $MAGISK_MODULE_BUILDER_DIR/*.patch ; do
     		PFILE=$(basename $i)
-		echo "PFILE=$PFILE"
+		echo "Patching $PFILE"
 		cp -f $i $PFILE
 		patch -p0 -i $PFILE
 		[ $? -ne 0 ] && { echo "Patching failed! Did you verify line numbers? See README for more info"; exit 1; }
@@ -80,6 +79,10 @@ magisk_step_patch_module() {
   	done
 }
 
-magisk_step_make_install() {
-	return
+magisk_step_post_make_install() {
+	mkdir -p $MAGISK_MODULE_MASSAGEDIR/$MAGISK_PREFIX/bin
+	ls $MAGISK_MODULE_SRCDIR
+	cp $MAGISK_MODULE_SRCDIR/bash $MAGISK_MODULE_MASSAGEDIR/$MAGISK_PREFIX/bin/bash
+	rm -Rf $MAGISK_MODULE_MASSAGEDIR/bin
+	tree $MAGISK_MODULE_MASSAGEDIR
 }
