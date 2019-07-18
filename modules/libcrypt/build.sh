28daa86f9bcd94b1701ab13837c0530cfd32fd13
henriknelson <henrik@cliffords.nu>
2019-07-18 14:43:06 +0200

    removed musl from libcrypt module

--- modules/libcrypt/build.sh
+++ modules/libcrypt/build.sh
@@ -5,10 +5,10 @@ MAGISK_MODULE_VERSION=0.2
 MAGISK_MODULE_REVISION=1
 MAGISK_MODULE_DEPENDS="openssl"
 
-magisk_step_pre_configure() {
-	CC=/usr/local/musl/bin/aarch64-linux-musl-gcc
-	LDFLAGS+="$LDFLAGS --static"
-}
+#magisk_step_pre_configure() {
+#	CC=/usr/local/musl/bin/aarch64-linux-musl-gcc
+#	LDFLAGS+="$LDFLAGS --static"
+#}
 
 magisk_step_make_install() {
 	$CC $CFLAGS $CPPFLAGS $LDFLAGS -Wall -Wextra -fPIC -shared $MAGISK_MODULE_BUILDER_DIR/crypt3.c -lcrypto -o $MAGISK_PREFIX/lib/libcrypt.so
