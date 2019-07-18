ddf0bfa8bb6fcd27878581efdd1093359fc71eb2
henriknelson <henrik@cliffords.nu>
2019-07-18 14:40:22 +0200

    removed musl from zlib

--- modules/zlib/build.sh
+++ modules/zlib/build.sh
@@ -5,13 +5,16 @@ MAGISK_MODULE_VERSION=1.2.11
 MAGISK_MODULE_REVISION=3
 MAGISK_MODULE_SHA256=4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066
 MAGISK_MODULE_SRCURL=https://www.zlib.net/zlib-$MAGISK_MODULE_VERSION.tar.xz
-MAGISK_MODULE_DEVPACKAGE_BREAKS="ndk-sysroot (<< 19b-3)"
-MAGISK_MODULE_DEVPACKAGE_REPLACES="ndk-sysroot (<< 19b-3)"
+MAGISK_MODULE_DEVMODULE_BREAKS="ndk-sysroot (<< 19b-3)"
+MAGISK_MODULE_DEVMODULE_REPLACES="ndk-sysroot (<< 19b-3)"
+MAGISK_MODULE_BUILD_IN_SRC=true
 
 magisk_step_configure() {
-	export PATH=/usr/local/musl/bin:$PATH
-	target=aarch64-linux-musl
-	export CC=$target-cc
-	export LD=$target-ld
-	"$MAGISK_MODULE_SRCDIR/configure" --prefix=$MAGISK_PREFIX --host=aarch64-linux-musl CC=$CC LD=$LD
+	#export PATH=/usr/local/musl/bin:$PATH
+	#target=aarch64-linux-musl
+	#export CC=$target-gcc
+	#export LD=$target-ld
+	#CC=$CC LD=$LD
+	./configure --prefix=$MAGISK_PREFIX --static --archs="-arch aarch64"
+	#--host=aarch64-linux-musl --target=aarch64-linux-musl
 }
