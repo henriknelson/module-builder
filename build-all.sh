dfd4713f8ee51240f757eab42bfd624279d09bde
henriknelson <henrik@cliffords.nu>
2019-07-21 23:57:58 +0200

    Fixed error in build-all script

--- build-all.sh
+++ build-all.sh
@@ -8,7 +8,7 @@ source scripts/build/magisk_docker_check.sh
 
 if [ ! "$(magisk_running_in_docker)" -eq 1 ]; then
         ./scripts/run-docker.sh $0 "$@"
-        return
+        exit 0
 fi
 
 # Read settings from .magiskrc if existing
