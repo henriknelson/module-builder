8bad6b27aded7cd531b6ec33cabe5b58b8fcdf28
Henrik Nelson <henrik@cliffords.nu>
2019-08-19 20:48:49 +0200

    Added module: libcaca

---
+++ modules/libcaca/build.sh
@@ -0,0 +1,9 @@
+MAGISK_MODULE_HOMEPAGE=http://caca.zoy.org/wiki/libcaca
+MAGISK_MODULE_DESCRIPTION="Graphics library that outputs text instead of pixels"
+MAGISK_MODULE_LICENSE="WTFPL"
+MAGISK_MODULE_VERSION=0.99.beta19
+MAGISK_MODULE_REVISION=6
+MAGISK_MODULE_SRCURL=http://fossies.org/linux/privat/libcaca-${MAGISK_MODULE_VERSION}.tar.gz
+MAGISK_MODULE_SHA256=128b467c4ed03264c187405172a4e83049342cc8cc2f655f53a2d0ee9d3772f4
+MAGISK_MODULE_DEPENDS="libc++, ncurses, zlib"
+MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--disable-python --disable-java --disable-ruby --disable-doc"
