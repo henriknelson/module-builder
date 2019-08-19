95dcd5b15f295a78dcb6b3f45da9678c1cc8b27a
Henrik Nelson <henrik@cliffords.nu>
2019-08-19 21:01:58 +0200

    Updated readline module

--- modules/readline/build.sh
+++ modules/readline/build.sh
@@ -1,21 +1,20 @@
 MAGISK_MODULE_HOMEPAGE="https://tiswww.case.edu/php/chet/readline/rltop.html"
 MAGISK_MODULE_DESCRIPTION="Library that allow users to edit command lines as they are typed in"
 MAGISK_MODULE_LICENSE="GPL-3.0"
-MAGISK_MODULE_DEPENDS="ncurses"
-MAGISK_MODULE_BREAKS="bash (<< 5.0)"
+MAGISK_MODULE_DEPENDS="libandroid-support, ncurses"
+MAGISK_MODULE_BREAKS="bash (<< 5.0), readline-dev"
+MAGISK_MODULE_REPLACES="readline-dev"
 MAGISK_MODULE_VERSION="8.0"
-MAGISK_MODULE_REVISION=2
+MAGISK_MODULE_REVISION=3
 MAGISK_MODULE_SHA256=e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461
 MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/readline/readline-${MAGISK_MODULE_VERSION}.tar.gz
-MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
---with-curses
---enable-static
---disable-shared
---enable-multibyte
---host=aarch64-linux-android
-bash_cv_wcwidth_broken=no"
+MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--with-curses --enable-multibyte bash_cv_wcwidth_broken=no"
 MAGISK_MODULE_EXTRA_MAKE_ARGS="SHLIB_LIBS=-lncursesw"
 MAGISK_MODULE_CONFFILES="etc/inputrc"
+
+magisk_step_pre_configure() {
+	CFLAGS+=" -fexceptions"
+}
 
 magisk_step_post_make_install() {
 	mkdir -p $MAGISK_PREFIX/lib/pkgconfig
