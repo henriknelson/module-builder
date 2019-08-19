9a56c86d6df4b150006b2a645a0d6728fabc2aba
Henrik Nelson <henrik@cliffords.nu>
2019-08-19 20:55:26 +0200

    Added patches for bash-completion

--- modules/bash-completion/build.sh
+++ modules/bash-completion/build.sh
@@ -4,7 +4,7 @@ MAGISK_MODULE_LICENSE="GPL-2.0"
 MAGISK_MODULE_VERSION=2.9
 MAGISK_MODULE_SHA256=d48fe378e731062f479c5f8802ffa9d3c40a275a19e6e0f6f6cc4b90fa12b2f5
 MAGISK_MODULE_SRCURL=https://github.com/scop/bash-completion/releases/download/${MAGISK_MODULE_VERSION}/bash-completion-${MAGISK_MODULE_VERSION}.tar.xz
-#MAGISK_MODULE_DEPENDS="bash"
+MAGISK_MODULE_DEPENDS="bash"
 MAGISK_MODULE_PLATFORM_INDEPENDENT=yes
 MAGISK_MODULE_BUILD_IN_SRC=yes
 
@@ -12,9 +12,9 @@ MAGISK_MODULE_BUILD_IN_SRC=yes
 
 magisk_step_configure() {
 	autoreconf -i
-	./configure --prefix=$MAGISK_PREFIX --host=aarch64-linux-android
+	$MAGISK_MODULE_SRCDIR/configure --prefix="$MAGISK_PREFIX" --host=aarch64-linux-android --target=aarch64-linux-android --datarootdir="$MAGISK_PREFIX/usr/share"
 }
 
-mmagisk_step_make_install() {
+magisk_step_make_install() {
 	make install
 }
