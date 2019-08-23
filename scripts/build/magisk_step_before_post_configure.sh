377cbd431362e73aeda186e818a04e1e38b7ab4f
Henrik Nelson <henrik@cliffords.nu>
2019-08-23 10:41:39 +0200

    Ensure any config.log file is transferred to modules debug directory, even if it is not created in source directory

--- scripts/build/magisk_step_before_post_configure.sh
+++ scripts/build/magisk_step_before_post_configure.sh
@@ -4,7 +4,13 @@ magisk_step_before_post_configure() {
 	magisk_log "checking for a config.log file.."
 	if [ -e "$MAGISK_MODULE_SRCDIR/config.log" ]; then
 		magisk_log "config.log found, copying to module script folder!"
-                cp $MAGISK_MODULE_SRCDIR/config.log $MAGISK_SCRIPTDIR/modules/$MAGISK_MODULE_NAME/config.log
+		mkdir -p $MAGISK_SCRIPTDIR/modules/$MAGISK_MODULE_NAME/debug
+                cp $MAGISK_MODULE_SRCDIR/config.log $MAGISK_SCRIPTDIR/modules/$MAGISK_MODULE_NAME/debug/config.log
+        elif [ -e "$MAGISK_MODULE_BUILDDIR/config.log" ]; then
+		magisk_log "config.log found, copying to module script folder!"
+		mkdir -p $MAGISK_SCRIPTDIR/modules/$MAGISK_MODULE_NAME/debug
+                cp $MAGISK_MODULE_BUILDDIR/config.log $MAGISK_SCRIPTDIR/modules/$MAGISK_MODULE_NAME/debug/config.log
         fi
+
 }
 
