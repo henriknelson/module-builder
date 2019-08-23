8430065b5039f46c635b5020058edd36dbff4d67
Henrik Nelson <henrik@cliffords.nu>
2019-08-23 10:42:01 +0200

    Enabled multiple submodules

---
+++ modules/liblua/lua.submodule.sh
@@ -0,0 +1,3 @@
+MAGISK_SUBMODULE_INCLUDE="bin/ share/man/man1/"
+MAGISK_SUBMODULE_DESCRIPTION="Simple, extensible, embeddable programming language"
+MAGISK_SUBMODULE_DEPENDS="readline"
---
+++ modules/nmap/netcat.submodule.sh
@@ -0,0 +1,3 @@
+MAGISK_SUBMODULE_INCLUDE="bin/ncat bin/netcat bin/nc usr/share/man/man1/ncat.1.gz usr/share/man/man1/netcat.1.gz usr/share/man/man1/nc.1.gz"
+MAGISK_SUBMODULE_DESCRIPTION="Feature-packed networking utility which reads and writes data across networks from the command line"
+MAGISK_SUBMODULE_DEPENDS="libpcap, openssl, liblua"
