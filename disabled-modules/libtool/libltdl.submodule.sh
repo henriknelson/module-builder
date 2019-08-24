7db3991ed03d7b54a94ece96f095bb69fbdf4617
Henrik Nelson <henrik@cliffords.nu>
2019-08-24 14:47:04 +0200

    Removed bad modules

---
+++ disabled-modules/libtool/libltdl.submodule.sh
@@ -0,0 +1,2 @@
+MAGISK_SUBMODULE_INCLUDE="lib/libltdl.so"
+MAGISK_SUBMODULE_DESCRIPTION="Library for dlopening libraries"
--- modules/libtool/libltdl.submodule.sh
+++
@@ -1,2 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="lib/libltdl.so"
-TERMUX_SUBPKG_DESCRIPTION="Library for dlopening libraries"
