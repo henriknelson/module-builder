2f446dda2193e08402af316c5b54e8b842122a48
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:18:48 +0200

    Updated module: pcre

--- modules/pcre/build.sh
+++ modules/pcre/build.sh
@@ -1,10 +1,10 @@
 MAGISK_MODULE_HOMEPAGE=https://www.pcre.org
 MAGISK_MODULE_DESCRIPTION="Library implementing regular expression pattern matching using the same syntax and semantics as Perl 5"
 MAGISK_MODULE_LICENSE="BSD 3-Clause"
-MAGISK_MODULE_VERSION=8.43
+MAGISK_MODULE_VERSION=8.44
 MAGISK_MODULE_REVISION=5
-MAGISK_MODULE_SRCURL=https://ftp.pcre.org/pub/pcre/pcre-${MAGISK_MODULE_VERSION}.tar.bz2
-MAGISK_MODULE_SHA256=91e762520003013834ac1adb4a938d53b22a216341c061b0cf05603b290faf6b
+MAGISK_MODULE_SRCURL=https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.bz2
+MAGISK_MODULE_SHA256=19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d
 MAGISK_MODULE_BREAKS="pcre-dev"
 MAGISK_MODULE_REPLACES="pcre-dev"
 MAGISK_MODULE_RM_AFTER_INSTALL="bin/pcregrep bin/pcretest share/man/man1/pcre*.1"
