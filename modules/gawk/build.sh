eb60f093556f2da9248cce2578b8e4b4d13ec801
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:17:19 +0200

    Updated module: gawk

--- modules/gawk/build.sh
+++ modules/gawk/build.sh
@@ -1,10 +1,10 @@
 MAGISK_MODULE_HOMEPAGE=https://www.gnu.org/software/gawk/
 MAGISK_MODULE_DESCRIPTION="Programming language designed for text processing"
 MAGISK_MODULE_LICENSE="GPL-2.0"
-MAGISK_MODULE_VERSION=5.0.1
+MAGISK_MODULE_VERSION=5.1.0
 MAGISK_MODULE_REVISION=2
-MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/gawk/gawk-${MAGISK_MODULE_VERSION}.tar.xz
-MAGISK_MODULE_SHA256=8e4e86f04ed789648b66f757329743a0d6dfb5294c3b91b756a474f1ce05a794
+MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/gawk/gawk-5.1.0.tar.xz
+MAGISK_MODULE_SHA256=cf5fea4ac5665fd5171af4716baab2effc76306a9572988d5ba1078f196382bd
 MAGISK_MODULE_DEPENDS="libandroid-support, libgmp, libmpfr, readline"
 MAGISK_MODULE_BREAKS="gawk-dev"
 MAGISK_MODULE_REPLACES="gawk-dev"
