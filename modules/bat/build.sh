ab0196a52f18ba032b6c8aa9af13211ef9c40bb1
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:16:47 +0200

    Updated module: bat

--- modules/bat/build.sh
+++ modules/bat/build.sh
@@ -1,10 +1,10 @@
 MAGISK_MODULE_HOMEPAGE=https://github.com/sharkdp/bat
 MAGISK_MODULE_DESCRIPTION="A cat(1) clone with wings"
 MAGISK_MODULE_LICENSE="Apache-2.0"
-MAGISK_MODULE_VERSION=0.11.0
+MAGISK_MODULE_VERSION=0.13.0
 MAGISK_MODULE_REVISION=2
-MAGISK_MODULE_SHA256=SKIP_CHECKSUM
-MAGISK_MODULE_SRCURL=https://github.com/sharkdp/bat/archive/v$MAGISK_MODULE_VERSION.zip
+MAGISK_MODULE_SHA256=f4aee370013e2a3bc84c405738ed0ab6e334d3a9f22c18031a7ea008cd5abd2a
+MAGISK_MODULE_SRCURL=https://github.com/sharkdp/bat/archive/v0.13.0.tar.gz
 MAGISK_MODULE_BUILD_IN_SRC=yes
 # bat calls less with '--RAW-CONTROL-CHARS' which busybox less does not support:
 #MAGISK_MODULE_DEPENDS="less"
