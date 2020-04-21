45c10fcfbd13a0f8f2f0d07966eac857b953a867
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:09:31 +0200

    Added module: libandroid-shmem

---
+++ modules/libandroid-shmem/build.sh
@@ -0,0 +1,9 @@
+MAGISK_MODULE_HOMEPAGE=https://github.com/termux/libandroid-shmem
+MAGISK_MODULE_DESCRIPTION="System V shared memory emulation on Android using ashmem"
+MAGISK_MODULE_LICENSE="BSD 3-Clause"
+MAGISK_MODULE_VERSION=0.2.1
+MAGISK_MODULE_SRCURL=https://github.com/termux/libandroid-shmem/archive/v${MAGISK_MODULE_VERSION}.tar.gz
+MAGISK_MODULE_SHA256=09369398658af357e3644178d42ebfcc79ea11fe760b10855011521fc3e35be4
+MAGISK_MODULE_BREAKS="libandroid-shmem-dev"
+MAGISK_MODULE_REPLACES="libandroid-shmem-dev"
+MAGISK_MODULE_BUILD_IN_SRC=true
