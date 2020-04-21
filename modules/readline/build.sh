86a938a1280bfee1f84ad5ba8b72e8f5f7f1a2ba
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:19:00 +0200

    Updated module: readline

--- modules/readline/build.sh
+++ modules/readline/build.sh
@@ -4,18 +4,36 @@ MAGISK_MODULE_LICENSE="GPL-3.0"
 MAGISK_MODULE_DEPENDS="libandroid-support, ncurses"
 MAGISK_MODULE_BREAKS="bash (<< 5.0), readline-dev"
 MAGISK_MODULE_REPLACES="readline-dev"
-MAGISK_MODULE_VERSION="8.0.1"
-MAGISK_MODULE_REVISION=3
+_MAIN_VERSION=8.0
+_PATCH_VERSION=4
+MAGISK_MODULE_VERSION=$_MAIN_VERSION.$_PATCH_VERSION
+MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/readline/readline-${_MAIN_VERSION}.tar.gz
 MAGISK_MODULE_SHA256=e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461
-MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/readline/readline-8.0.tar.gz
-MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--prefix=$MAGISK_PREFIX --host=aarch64-linux-android --with-curses --enable-multibyte bash_cv_wcwidth_broken=no --enable-static --disable-shared"
-MAGISK_MODULE_EXTRA_MAKE_ARGS="SHLIB_LIBS=-lncursesw -lncurses"
+MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--with-curses --enable-multibyte bash_cv_wcwidth_broken=no --enable-static --disable-shared"
+#MAGISK_MODULE_EXTRA_MAKE_ARGS="SHLIB_LIBS=-lncursesw"
 MAGISK_MODULE_CONFFILES="etc/inputrc"
 
+magisk_step_pre_configure() {
+	declare -A PATCH_CHECKSUMS
+	PATCH_CHECKSUMS[001]=d8e5e98933cf5756f862243c0601cb69d3667bb33f2c7b751fe4e40b2c3fd069
+	PATCH_CHECKSUMS[002]=36b0febff1e560091ae7476026921f31b6d1dd4c918dcb7b741aa2dad1aec8f7
+	PATCH_CHECKSUMS[003]=94ddb2210b71eb5389c7756865d60e343666dfb722c85892f8226b26bb3eeaef
+	PATCH_CHECKSUMS[004]=b1aa3d2a40eee2dea9708229740742e649c32bb8db13535ea78f8ac15377394c
+	for PATCH_NUM in $(seq -f '%03g' ${_PATCH_VERSION}); do
+		PATCHFILE=$MAGISK_MODULE_CACHEDIR/readline_patch_${PATCH_NUM}.patch
+		magisk_download \
+			"http://mirrors.kernel.org/gnu/readline/readline-$_MAIN_VERSION-patches/readline${_MAIN_VERSION/./}-$PATCH_NUM" \
+			$PATCHFILE \
+			${PATCH_CHECKSUMS[$PATCH_NUM]}
+		patch -p0 -i $PATCHFILE
+	done
+
+	CFLAGS+=" -fexceptions -static"
+	LDFLAGS+=" --static"
+}
+
 mmagisk_step_pre_configure() {
-	CFLAGS+=" -static"
-	LDFLAGS+=" --static -landroid-support -ltinfo -ltermcap -lncurses"
-	#LDFLAGS+=" --static"
+exit
 }
 
 magisk_step_post_make_install() {
---
+++ modules/readline/build.sh2
@@ -0,0 +1,40 @@
+TERMUX_PKG_HOMEPAGE="https://tiswww.case.edu/php/chet/readline/rltop.html"
+TERMUX_PKG_DESCRIPTION="Library that allow users to edit command lines as they are typed in"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+TERMUX_PKG_BREAKS="bash (<< 5.0), readline-dev"
+TERMUX_PKG_REPLACES="readline-dev"
+_MAIN_VERSION=8.0
+_PATCH_VERSION=4
+TERMUX_PKG_VERSION=$_MAIN_VERSION.$_PATCH_VERSION
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/readline/readline-${_MAIN_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-curses --enable-multibyte bash_cv_wcwidth_broken=no"
+TERMUX_PKG_EXTRA_MAKE_ARGS="SHLIB_LIBS=-lncursesw"
+TERMUX_PKG_CONFFILES="etc/inputrc"
+
+termux_step_pre_configure() {
+	declare -A PATCH_CHECKSUMS
+	PATCH_CHECKSUMS[001]=d8e5e98933cf5756f862243c0601cb69d3667bb33f2c7b751fe4e40b2c3fd069
+	PATCH_CHECKSUMS[002]=36b0febff1e560091ae7476026921f31b6d1dd4c918dcb7b741aa2dad1aec8f7
+	PATCH_CHECKSUMS[003]=94ddb2210b71eb5389c7756865d60e343666dfb722c85892f8226b26bb3eeaef
+	PATCH_CHECKSUMS[004]=b1aa3d2a40eee2dea9708229740742e649c32bb8db13535ea78f8ac15377394c
+	for PATCH_NUM in $(seq -f '%03g' ${_PATCH_VERSION}); do
+		PATCHFILE=$TERMUX_PKG_CACHEDIR/readline_patch_${PATCH_NUM}.patch
+		termux_download \
+			"http://mirrors.kernel.org/gnu/readline/readline-$_MAIN_VERSION-patches/readline${_MAIN_VERSION/./}-$PATCH_NUM" \
+			$PATCHFILE \
+			${PATCH_CHECKSUMS[$PATCH_NUM]}
+		patch -p0 -i $PATCHFILE
+	done
+
+	CFLAGS+=" -fexceptions"
+}
+
+termux_step_post_make_install() {
+	mkdir -p $TERMUX_PREFIX/lib/pkgconfig
+	cp readline.pc $TERMUX_PREFIX/lib/pkgconfig/
+
+	mkdir -p $TERMUX_PREFIX/etc
+	cp $TERMUX_PKG_BUILDER_DIR/inputrc $TERMUX_PREFIX/etc/
+}
