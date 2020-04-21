3b775af8552f2133b68426f0d721c5efd9a23cc6
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:17:15 +0200

    Updated module: findutils

--- modules/findutils/build.sh
+++ modules/findutils/build.sh
@@ -1,10 +1,10 @@
 MAGISK_MODULE_HOMEPAGE=https://www.gnu.org/software/findutils/
 MAGISK_MODULE_DESCRIPTION="Utilities to find files meeting specified criteria and perform various actions on the files which are found"
 MAGISK_MODULE_LICENSE="GPL-3.0"
-MAGISK_MODULE_VERSION=4.6.0
+MAGISK_MODULE_VERSION=4.7.0
 MAGISK_MODULE_REVISION=4
-MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/findutils/findutils-${MAGISK_MODULE_VERSION}.tar.gz
-MAGISK_MODULE_SHA256=ded4c9f73731cd48fec3b6bdaccce896473b6d8e337e9612e16cf1431bb1169d
+MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/findutils/findutils-4.7.0.tar.xz
+MAGISK_MODULE_SHA256=c5fefbdf9858f7e4feb86f036e1247a54c79fc2d8e4b7064d5aaa1f47dfa789a
 MAGISK_MODULE_DEPENDS="libandroid-support"
 MAGISK_MODULE_ESSENTIAL=true
 
