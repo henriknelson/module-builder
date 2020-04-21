a72793b90fff5c715ff1999b8d0c591f16f1ae5b
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:09:39 +0200

    Added module: libpipeline

---
+++ modules/libpipeline/build.sh
@@ -0,0 +1,9 @@
+MAGISK_MODULE_HOMEPAGE=http://libpipeline.nongnu.org/
+MAGISK_MODULE_DESCRIPTION="C library for manipulating pipelines of subprocesses in a flexible and convenient way"
+MAGISK_MODULE_LICENSE="GPL-2.0"
+MAGISK_MODULE_VERSION=1.5.2
+MAGISK_MODULE_REVISION=1
+MAGISK_MODULE_SHA256=fd59c649c1ae9d67604d1644f116ad4d297eaa66f838e3dfab96b41e85b059fb
+MAGISK_MODULE_BREAKS="libpipeline-dev"
+MAGISK_MODULE_REPLACES="libpipeline-dev"
+MAGISK_MODULE_SRCURL=http://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.5.2.tar.gz
