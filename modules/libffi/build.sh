ca7f8b1a42dabb94caebf4e1f4ca25933d5532d6
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:18:01 +0200

    Updated module: libffi

--- modules/libffi/build.sh
+++ modules/libffi/build.sh
@@ -1,10 +1,10 @@
 MAGISK_MODULE_HOMEPAGE=https://sourceware.org/libffi/
 MAGISK_MODULE_DESCRIPTION="Library providing a portable, high level programming interface to various calling conventions"
 MAGISK_MODULE_LICENSE="MIT"
-MAGISK_MODULE_VERSION=3.2.1
+MAGISK_MODULE_VERSION=3.3
 MAGISK_MODULE_REVISION=3
-MAGISK_MODULE_SRCURL=ftp://sourceware.org/pub/libffi/libffi-${MAGISK_MODULE_VERSION}.tar.gz
-MAGISK_MODULE_SHA256=d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37
+MAGISK_MODULE_SRCURL=ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz
+MAGISK_MODULE_SHA256=72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
 --disable-multi-os-directory
 "
