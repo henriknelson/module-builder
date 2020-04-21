8182d2ac68cbeeb34d8b79dcc15d4b8630ffee78
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:17:11 +0200

    Updated module: fd

--- modules/fd/build.sh
+++ modules/fd/build.sh
@@ -1,25 +1,12 @@
 MAGISK_MODULE_HOMEPAGE=https://github.com/sharkdp/fd
 MAGISK_MODULE_DESCRIPTION="Simple, fast and user-friendly alternative to find"
 MAGISK_MODULE_LICENSE="Apache-2.0"
-MAGISK_MODULE_VERSION=7.3.0
-MAGISK_MODULE_REVISION=1
-MAGISK_MODULE_SHA256=fbd48cc83c90a0ab09fc3bbe865708a3a528876a99f8304a17d07af7fb378170
+MAGISK_MODULE_VERSION=8.0.0
 MAGISK_MODULE_SRCURL=https://github.com/sharkdp/fd/archive/v$MAGISK_MODULE_VERSION.tar.gz
-MAGISK_MODULE_BUILD_IN_SRC=yes
+MAGISK_MODULE_SHA256=fba93204c10266317e0981914c630b08e12cd322c75ff2a2e504ff1dce17d557
+MAGISK_MODULE_BUILD_IN_SRC=true
 
-
-#magisk_step_patch_module() {
-#	cd $MAGISK_MODULE_SRCDIR
-#	echo "$MAGISK_MODULE_BUILDER_DIR"
-#	for i in $MAGISK_MODULE_BUILDER_DIR/*.patch ; do
-#    		PFILE=$(basename $i)
-#		echo "PFILE=$PFILE"
-#		cp -f $i $PFILE
-#		echo $(pwd)
-#		ls
-#		patch -p0 -i $PFILE
-#		[ $? -ne 0 ] && { echo "Patching failed! Did you verify line numbers? See README for more info"; exit 1; }
-#		#rm -f $PFILE
- # 	done
-#}
-
+termux_step_post_make_install() {
+	mkdir -p  $MAGISK_PREFIX/share/man/man1
+	cp $MAGISK_MODULE_SRCDIR/doc/fd.1 $MAGISK_PREFIX/share/man/man1/fd.1
+}
