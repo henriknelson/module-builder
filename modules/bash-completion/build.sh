0e38c473a8330de297b8933225e77035edde65e2
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:16:39 +0200

    Updated module: bash-completion

--- modules/bash-completion/build.sh
+++ modules/bash-completion/build.sh
@@ -1,9 +1,9 @@
 MAGISK_MODULE_HOMEPAGE=https://github.com/scop/bash-completion
 MAGISK_MODULE_DESCRIPTION="Programmable completion for the bash shell"
 MAGISK_MODULE_LICENSE="GPL-2.0"
-MAGISK_MODULE_VERSION=2.9
-MAGISK_MODULE_SHA256=d48fe378e731062f479c5f8802ffa9d3c40a275a19e6e0f6f6cc4b90fa12b2f5
-MAGISK_MODULE_SRCURL=https://github.com/scop/bash-completion/releases/download/${MAGISK_MODULE_VERSION}/bash-completion-${MAGISK_MODULE_VERSION}.tar.xz
+MAGISK_MODULE_VERSION=2.10
+MAGISK_MODULE_SHA256=123c17998e34b937ce57bb1b111cd817bc369309e9a8047c0bcf06ead4a3ec92
+MAGISK_MODULE_SRCURL=https://github.com/scop/bash-completion/releases/download/2.10/bash-completion-2.10.tar.xz
 MAGISK_MODULE_DEPENDS="bash"
 MAGISK_MODULE_PLATFORM_INDEPENDENT=yes
 MAGISK_MODULE_BUILD_IN_SRC=yes
