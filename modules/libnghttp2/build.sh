9a67da52b18b4e07169d01eb77d0dc46dabd2e64
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:18:13 +0200

    Updated module: libnghttp2

--- modules/libnghttp2/build.sh
+++ modules/libnghttp2/build.sh
@@ -1,9 +1,9 @@
 MAGISK_MODULE_HOMEPAGE=https://nghttp2.org/
 MAGISK_MODULE_DESCRIPTION="nghttp HTTP 2.0 library"
 MAGISK_MODULE_LICENSE="MIT"
-MAGISK_MODULE_VERSION=1.39.2
-MAGISK_MODULE_SRCURL=https://github.com/nghttp2/nghttp2/releases/download/v1.39.2/nghttp2-1.39.2.tar.xz
-MAGISK_MODULE_SHA256=a2d216450abd2beaf4e200c168957968e89d602ca4119338b9d7ab059fd4ce8b
+MAGISK_MODULE_VERSION=1.40.0
+MAGISK_MODULE_SRCURL=https://github.com/nghttp2/nghttp2/releases/download/v1.40.0/nghttp2-1.40.0.tar.xz
+MAGISK_MODULE_SHA256=09fc43d428ff237138733c737b29fb1a7e49d49de06d2edbed3bc4cdcee69073
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
 --enable-lib-only
 --disable-shared
