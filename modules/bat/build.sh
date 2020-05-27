3fe83491e02bfae2da1ebdf3151e0f08b517913a
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:19:35 +0200

    Updated module: bat

--- modules/bat/build.sh
+++ modules/bat/build.sh
@@ -1,10 +1,24 @@
 MAGISK_MODULE_HOMEPAGE=https://github.com/sharkdp/bat
 MAGISK_MODULE_DESCRIPTION="A cat(1) clone with wings"
 MAGISK_MODULE_LICENSE="Apache-2.0"
-MAGISK_MODULE_VERSION=0.13.0
-MAGISK_MODULE_REVISION=2
-MAGISK_MODULE_SHA256=f4aee370013e2a3bc84c405738ed0ab6e334d3a9f22c18031a7ea008cd5abd2a
-MAGISK_MODULE_SRCURL=https://github.com/sharkdp/bat/archive/v0.13.0.tar.gz
-MAGISK_MODULE_BUILD_IN_SRC=yes
+MAGISK_MODULE_VERSION=0.15.4
+MAGISK_MODULE_SRCURL=https://github.com/sharkdp/bat/archive/v${MAGISK_MODULE_VERSION}.tar.gz
+MAGISK_MODULE_SHA256=03b7c8ad6221ca87cecd71f9e3e2167f04f750401e2d3dcc574183aabeb76a8b
 # bat calls less with '--RAW-CONTROL-CHARS' which busybox less does not support:
-#MAGISK_MODULE_DEPENDS="less"
+MAGISK_MODULE_DEPENDS="less, zlib"
+MAGISK_MODULE_BUILD_IN_SRC=true
+
+magisk_step_pre_configure() {
+	CFLAGS="$CFLAGS -I/system/include"
+	LDFLAGS="-lz $LDFLAGS -L/system/lib"
+	# $CPPFLAGS"
+
+	# See https://github.com/nagisa/rust_libloading/issues/54
+	export CC_x86_64_unknown_linux_gnu=gcc
+	export CFLAGS_x86_64_unknown_linux_gnu=""
+}
+
+magisk_step_post_make_install() {
+	mkdir -p $MAGISK_PREFIX/usr/share/man/man1
+	cp $(find . -name bat.1) $MAGISK_PREFIX/usr/share/man/man1/
+}
