087394c8ed83bb90c5eb2ecde109e65f9c09714d
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:19:35 +0200

    Updated module: neofetch

--- modules/neofetch/build.sh
+++ modules/neofetch/build.sh
@@ -4,7 +4,7 @@ MAGISK_MODULE_LICENSE="MIT"
 MAGISK_MODULE_MAINTAINER="Muhammad Herdiansyah @konimex"
 MAGISK_MODULE_VERSION=7.0.0
 MAGISK_MODULE_SHA256=8c6bd217cf6d34fc1f3dcbb0e8b1137655bc13fbb21165273dbb2a7bce0d3130
-MAGISK_MODULE_SRCURL=https://github.com/dylanaraps/neofetch/archive/7.0.0/neofetch-7.0.0.tar.gz
+MAGISK_MODULE_SRCURL=https://github.com/dylanaraps/neofetch/archive/${MAGISK_MODULE_VERSION}/neofetch-${MAGISK_MODULE_VERSION}.tar.gz
 MAGISK_MODULE_BUILD_IN_SRC=yes
 MAGISK_MODULE_PLATFORM_INDEPENDENT=yes
 
