e36598e5747e149ccc127d6233bb358c76f565f3
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:09:44 +0200

    Added xcb-proto/ module

---
+++ modules/xcb-proto/build.sh
@@ -0,0 +1,13 @@
+# X11 package
+MAGISK_MODULE_HOMEPAGE=https://xcb.freedesktop.org/
+MAGISK_MODULE_DESCRIPTION="XML-XCB protocol descriptions"
+MAGISK_MODULE_LICENSE="MIT"
+MAGISK_MODULE_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+MAGISK_MODULE_VERSION=1.14
+MAGISK_MODULE_REVISION=1
+MAGISK_MODULE_SRCURL=https://ftp.osuosl.org/pub/blfs/svn/Xorg/xcb-proto-$MAGISK_MODULE_VERSION.tar.xz
+#https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-$MAGISK_MODULE_VERSION.tar.xz
+MAGISK_MODULE_SHA256=186a3ceb26f9b4a015f5a44dcc814c93033a5fc39684f36f1ecc79834416a605
+MAGISK_MODULE_PLATFORM_INDEPENDENT=true
+MAGISK_MODULE_CONFLICTS="xcbproto"
+MAGISK_MODULE_REPLACES="xcbproto"
