350c9bc9a85e2197c273613ea24e0da236478dc5
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:19:35 +0200

    Updated module: libgmp

--- modules/libgmp/build.sh
+++ modules/libgmp/build.sh
@@ -3,13 +3,14 @@ MAGISK_MODULE_DESCRIPTION="Library for arbitrary precision arithmetic"
 MAGISK_MODULE_LICENSE="LGPL-3.0"
 MAGISK_MODULE_VERSION=6.2.0
 MAGISK_MODULE_REVISION=4
-MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/gmp/gmp-6.2.0.tar.xz
+MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/gmp/gmp-${MAGISK_MODULE_VERSION}.tar.xz
 MAGISK_MODULE_SHA256=258e6cd51b3fbdfc185c716d55f82c08aff57df0c6fbd143cf6ed561267a1526
 MAGISK_MODULE_BREAKS="libgmp-dev"
 MAGISK_MODULE_REPLACES="libgmp-dev"
-MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--enable-cxx"
+MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--enable-cxx --enable-shared"
 
-magisk_step_pre_configure() {
+mmagisk_step_pre_configure() {
 # the cxx tests fail because it won't link properly without this
+    CXX=$CC
     CXXFLAGS+=" -L$MAGISK_PREFIX/lib"
 }
