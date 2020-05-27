5a7bdb952833c159ec326b2424abcc40e6af83d6
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:19:35 +0200

    Updated module: man

--- modules/man/build.sh
+++ modules/man/build.sh
@@ -8,19 +8,24 @@ MAGISK_MODULE_SRCURL=http://mdocml.bsd.lv/snapshots/mandoc-${MAGISK_MODULE_VERSI
 MAGISK_MODULE_DEPENDS="less,libandroid-glob,zlib"
 MAGISK_MODULE_BUILD_IN_SRC=yes
 MAGISK_MODULE_RM_AFTER_INSTALL="usr/share/examples"
+MAGISK_MODULE_EXTRA_CONFIGURE_ARGS=" --enable-shared"
 
 magisk_step_pre_configure() {
-	LDFLAGS+=" -static -landroid-glob -lz -lc"
+	#sudo rm $MAGISK_MODULE_SRCDIR/compat_*;
+	#LDFLAGS+=" -static"
 	echo "PREFIX=\"$MAGISK_PREFIX\"" > configure.local
 	echo "CC=\"$CC\"" >> configure.local
+	echo "LD=\"$CC\"" >> configure.local
 	echo "MANDIR=\"/data/man\"" >> configure.local
-	echo "CFLAGS=\"$CFLAGS --static -DNULL=0 $CPPFLAGS\"" >> configure.local
-	echo "LDFLAGS=\"$LDFLAGS\"" >> configure.local
-	#echo "LDADD=\" -lz -lc -landroid-glob\"" >> configure.local
-	for HAVING in HAVE_FGETLN HAVE_MMAP HAVE_STRLCAT HAVE_STRLCPY HAVE_SYS_ENDIAN HAVE_ENDIAN HAVE_NTOHL HAVE_NANOSLEEP HAVE_O_DIRECTORY; do
+	#echo "CFLAGS=\"$CFLAGS -DNULL=0\"" >> configure.local
+	echo "LDFLAGS=\"$LDFLAGS --static\"" >> configure.local
+	#echo "STATIC=\"-static\"" >> configure.local
+	echo "LDADD=\" -landroid-glob -lc\"" >> configure.local
+	for HAVING in HAVE_REALLOCARRAY HAVE_GETLINE HAVE_FGETLN HAVE_MMAP HAVE_STRLCAT HAVE_STRLCPY HAVE_SYS_ENDIAN HAVE_ENDIAN HAVE_NTOHL HAVE_NANOSLEEP HAVE_O_DIRECTORY HAVE_MKDTEMP; do
 		echo "$HAVING=1" >> configure.local
 	done
 	echo "HAVE_MANPATH=0" >> configure.local
+	echo "HAVE_STRINGLIST=0" >> configure.local
 	echo "HAVE_SQLITE3=1" >> configure.local
 	printenv >> config.env
 }
