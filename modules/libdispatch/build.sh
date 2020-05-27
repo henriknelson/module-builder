1a3b82e52f04753fbc26f1bae0af1f74e92c5be8
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:09:44 +0200

    Added libdispatch/ module

---
+++ modules/libdispatch/build.sh
@@ -0,0 +1,8 @@
+MAGISK_MODULE_HOMEPAGE=https://github.com/apple/swift-corelibs-libdspatch
+MAGISK_MODULE_DESCRIPTION="The libdispatch project, for concurrency on multicore hardware"
+MAGISK_MODULE_LICENSE="Apache-2.0"
+_VERSION=5.2
+MAGISK_MODULE_VERSION=1:${_VERSION}
+MAGISK_MODULE_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${_VERSION}-RELEASE.tar.gz
+MAGISK_MODULE_SHA256=11797a43c509a603434c64c1041f208f0f25c3233d6c6493d53704e682634edb
+MAGISK_MODULE_DEPENDS="libc++"
