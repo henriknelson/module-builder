a5cfe529623ab6b4e8568bf32d8df690eb91a7cf
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:09:44 +0200

    Added libxft/ module

---
+++ modules/libxft/build.sh
@@ -0,0 +1,10 @@
+# X11 package
+MAGISK_MODULE_HOMEPAGE=https://xorg.freedesktop.org/
+MAGISK_MODULE_DESCRIPTION="FreeType-based font drawing library for X"
+MAGISK_MODULE_LICENSE="MIT"
+MAGISK_MODULE_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+MAGISK_MODULE_VERSION=2.3.3
+MAGISK_MODULE_REVISION=10
+MAGISK_MODULE_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXft-${MAGISK_MODULE_VERSION}.tar.bz2
+MAGISK_MODULE_SHA256=225c68e616dd29dbb27809e45e9eadf18e4d74c50be43020ef20015274529216
+MAGISK_MODULE_DEPENDS="fontconfig, freetype, libbz2, liblzma, libpng, libuuid, libx11, libxau, libxcb, libxdmcp, libxml2, libxrender"
