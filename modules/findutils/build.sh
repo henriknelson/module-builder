921aeec08c8bd5c5ab1dd35d4ef1d6133a6b6f38
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:08:22 +0200

    Updated findutils module

--- modules/findutils/build.sh
+++ modules/findutils/build.sh
@@ -2,8 +2,7 @@ MAGISK_MODULE_HOMEPAGE=https://www.gnu.org/software/findutils/
 MAGISK_MODULE_DESCRIPTION="Utilities to find files meeting specified criteria and perform various actions on the files which are found"
 MAGISK_MODULE_LICENSE="GPL-3.0"
 MAGISK_MODULE_VERSION=4.7.0
-MAGISK_MODULE_REVISION=4
-MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/findutils/findutils-4.7.0.tar.xz
+MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/findutils/findutils-${MAGISK_MODULE_VERSION}.tar.xz
 MAGISK_MODULE_SHA256=c5fefbdf9858f7e4feb86f036e1247a54c79fc2d8e4b7064d5aaa1f47dfa789a
 MAGISK_MODULE_DEPENDS="libandroid-support"
 MAGISK_MODULE_ESSENTIAL=true
@@ -27,4 +26,5 @@ magisk_step_pre_configure() {
 	# This is needed for find to implement support for the
 	# -fstype parameter by parsing /proc/self/mountinfo:
 	CPPFLAGS+=" -DMOUNTED_GETMNTENT1=1"
+	LDFLAGS+=" -static"
 }
