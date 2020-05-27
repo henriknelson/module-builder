e1d7902248ec8ee2b6b6b123674e92401a2e5ab0
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:08:43 +0200

    Updated hyperfine module

--- modules/hyperfine/build.sh
+++ modules/hyperfine/build.sh
@@ -2,8 +2,12 @@ MAGISK_MODULE_HOMEPAGE=https://github.com/sharkdp/hyperfine
 MAGISK_MODULE_DESCRIPTION="A command-line benchmarking tool"
 MAGISK_MODULE_LICENSE="Apache-2.0"
 MAGISK_MODULE_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-MAGISK_MODULE_VERSION=1.9.0
-MAGISK_MODULE_REVISION=1
-MAGISK_MODULE_SRCURL=https://github.com/sharkdp/hyperfine/archive/v1.9.0.tar.gz
-MAGISK_MODULE_SHA256=c2b1c6b6364b849acad43dc740c693f5b75a1a4649f24f43967a98c59ad9e9f7
+MAGISK_MODULE_VERSION=1.10.0
+MAGISK_MODULE_SRCURL=https://github.com/sharkdp/hyperfine/archive/v${MAGISK_MODULE_VERSION}.tar.gz
+MAGISK_MODULE_SHA256=b949d6c1a78e9c1c5a7bb6c241fcd51d6faf00bba5719cc312f57b5b301cc854
 MAGISK_MODULE_BUILD_IN_SRC=yes
+
+magisk_step_post_make_install() {
+        mkdir -p $MAGISK_PREFIX/usr/share/man/man1
+        cp $(find . -name hyperfine.1) $MAGISK_PREFIX/usr/share/man/man1/
+}
