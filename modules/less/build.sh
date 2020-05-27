c2460e6092c283d75b73ecbba9f611d72b4bda8d
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:08:13 +0200

    Updated less module

--- modules/less/build.sh
+++ modules/less/build.sh
@@ -1,11 +1,15 @@
 MAGISK_MODULE_HOMEPAGE=http://www.greenwoodsoftware.com/less/
 MAGISK_MODULE_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 MAGISK_MODULE_LICENSE="GPL-3.0"
-MAGISK_MODULE_VERSION=551
+MAGISK_MODULE_VERSION=561
 MAGISK_MODULE_REVISION=1
 MAGISK_MODULE_SRCURL=http://www.greenwoodsoftware.com/less/less-${MAGISK_MODULE_VERSION}.tar.gz
-MAGISK_MODULE_SHA256=ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d
+MAGISK_MODULE_SHA256=c24683c98d5f377adc54a0c28e84391346a98f7d6fd0bb0df1831c8a5cfc9836
 MAGISK_MODULE_DEPENDS="ncurses"
+
+magisk_step_post_extract_module() {
+	sudo chown -R builder:builder .
+}
 
 magisk_step_pre_configure(){
 	CFLAGS+=" -static"
