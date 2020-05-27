6a56457c9e7d6273d7bf323e9661a193e49b33c5
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:19:35 +0200

    Updated module: fd

--- modules/fd/build.sh
+++ modules/fd/build.sh
@@ -1,12 +1,13 @@
 MAGISK_MODULE_HOMEPAGE=https://github.com/sharkdp/fd
 MAGISK_MODULE_DESCRIPTION="Simple, fast and user-friendly alternative to find"
 MAGISK_MODULE_LICENSE="Apache-2.0"
-MAGISK_MODULE_VERSION=8.0.0
-MAGISK_MODULE_SRCURL=https://github.com/sharkdp/fd/archive/v$MAGISK_MODULE_VERSION.tar.gz
-MAGISK_MODULE_SHA256=fba93204c10266317e0981914c630b08e12cd322c75ff2a2e504ff1dce17d557
+MAGISK_MODULE_VERSION=8.1.1
+MAGISK_MODULE_SRCURL=https://github.com/sharkdp/fd/archive/v8.1.1.tar.gz
+MAGISK_MODULE_SHA256=7b327dc4c2090b34c7fb3e5ac7147f7bbe6266c2d44b182038d36f3b1d347cc1
 MAGISK_MODULE_BUILD_IN_SRC=true
 
-termux_step_post_make_install() {
-	mkdir -p  $MAGISK_PREFIX/share/man/man1
-	cp $MAGISK_MODULE_SRCDIR/doc/fd.1 $MAGISK_PREFIX/share/man/man1/fd.1
+magisk_step_post_make_install() {
+        mkdir -p $MAGISK_PREFIX/usr/share/man/man1
+        cp $(find . -name fd.1) $MAGISK_PREFIX/usr/share/man/man1/
 }
+
