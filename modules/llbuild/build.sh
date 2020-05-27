83d69c0988fe06de37fec405c48561a7a85124b3
Henrik Nelson <henrik@cliffords.nu>
2020-05-27 19:09:44 +0200

    Added llbuild/ module

---
+++ modules/llbuild/build.sh
@@ -0,0 +1,7 @@
+MAGISK_MODULE_HOMEPAGE=https://github.com/apple/swift-llbuild
+MAGISK_MODULE_DESCRIPTION="A low-level build system, used by the Swift Package Manager"
+MAGISK_MODULE_LICENSE="Apache-2.0, NCSA"
+MAGISK_MODULE_VERSION=5.2
+MAGISK_MODULE_SRCURL=https://github.com/apple/swift-llbuild/archive/swift-${MAGISK_MODULE_VERSION}-RELEASE.tar.gz
+MAGISK_MODULE_SHA256=6c781a4f7887a10e0443eaebde4d5f52333417392ed2422f012917c879014224
+MAGISK_MODULE_DEPENDS="libc++, libandroid-spawn, libsqlite"
