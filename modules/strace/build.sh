493de183bbd3563919e9263874a9dc96843ae892
Henrik Nelson <henrik@cliffords.nu>
2020-07-07 14:42:19 +0200

    Updated strace build script to build version 5.7

--- modules/strace/build.sh
+++ modules/strace/build.sh
@@ -1,18 +1,17 @@
 MAGISK_MODULE_HOMEPAGE=https://strace.io/
 MAGISK_MODULE_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 MAGISK_MODULE_LICENSE="BSD"
-MAGISK_MODULE_VERSION=5.6
+MAGISK_MODULE_VERSION=5.7
 MAGISK_MODULE_SRCURL=https://github.com/strace/strace/releases/download/v$MAGISK_MODULE_VERSION/strace-$MAGISK_MODULE_VERSION.tar.xz
-MAGISK_MODULE_SHA256=189968eeae06ed9e20166ec55a830943c84374676a457c9fe010edc7541f1b01
+MAGISK_MODULE_SHA256=b284b59f9bcd95b9728cea5bd5c0edc5ebe360af73dc76fbf6334f11c777ccd8
 MAGISK_MODULE_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
 st_cv_m32_mpers=no
 --enable-mpers=no
---without-libdw
 "
 
 magisk_step_pre_configure() {
 	CPPFLAGS+=" -Dfputs_unlocked=fputs"
-	LDFLAGS+=" -static"
+	LDFLAGS+=" -static -pthread"
 }
