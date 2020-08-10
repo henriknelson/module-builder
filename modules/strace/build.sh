b42b16c463d7aae358aa7ad0408d3bf72b17d441
Henrik Nelson <henrik@cliffords.nu>
2020-08-10 09:04:43 +0200

    Updated strace to v. 5.8

--- modules/strace/build.sh
+++ modules/strace/build.sh
@@ -1,9 +1,10 @@
 MAGISK_MODULE_HOMEPAGE=https://strace.io/
 MAGISK_MODULE_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 MAGISK_MODULE_LICENSE="BSD"
-MAGISK_MODULE_VERSION=5.7
+MAGISK_MODULE_VERSION=5.8
 MAGISK_MODULE_SRCURL=https://github.com/strace/strace/releases/download/v$MAGISK_MODULE_VERSION/strace-$MAGISK_MODULE_VERSION.tar.xz
-MAGISK_MODULE_SHA256=b284b59f9bcd95b9728cea5bd5c0edc5ebe360af73dc76fbf6334f11c777ccd8
+MAGISK_MODULE_SHA256=df4a669f7fff9cc302784085bd4b72fab216a426a3f72c892b28a537b71e7aa9
+#MAGISK_MODULE_DEPENDS="libdw"
 MAGISK_MODULE_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
---
+++ modules/strace/build.tmux
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://strace.io/
+TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_VERSION=5.8
+TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=df4a669f7fff9cc302784085bd4b72fab216a426a3f72c892b28a537b71e7aa9
+TERMUX_PKG_DEPENDS="libdw"
+
+# Without st_cv_m32_mpers=no the build fails if gawk is installed.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+st_cv_m32_mpers=no
+--enable-mpers=no
+--with-libdw
+"
+
+# This is a perl script.
+TERMUX_PKG_RM_AFTER_INSTALL="bin/strace-graph"
+
+termux_step_pre_configure() {
+	CPPFLAGS+=" -Dfputs_unlocked=fputs"
+}
