1c6a075cde1012819901dd05a9b47c8f05c82ea4
Henrik Nelson <henrik@cliffords.nu>
2020-10-15 00:29:59 +0200

    Added patch to github-cli module which allows a browser to spawn from an Andriod shell during the applications github authentication process

--- modules/gh/build.sh
+++ modules/gh/build.sh
@@ -5,6 +5,7 @@ MAGISK_MODULE_MAINTAINER="Krishna kanhaiya @kcubeterm"
 MAGISK_MODULE_VERSION=1.1.0
 MAGISK_MODULE_SRCURL=https://github.com/cli/cli/archive/v${MAGISK_MODULE_VERSION}.tar.gz
 MAGISK_MODULE_SHA256=3340bcec77dd9cbb5074399effc876cb7d759a235d24ea11253ad7b232b620a2
+
 magisk_step_make() {
 	magisk_setup_golang
 
@@ -18,6 +19,8 @@ magisk_step_make() {
 	mkdir -p "$GOPATH"/src/github.com/cli/
 	mkdir -p "$MAGISK_PREFIX"/share/doc/gh
 	cp -a "$MAGISK_MODULE_SRCDIR" "$GOPATH"/src/github.com/cli/cli
+	#sudo chown builder:builder -R $GOPATH/..
+	#sudo chmod 755 -R $GOPATH/..
 	cd "$GOPATH"/src/github.com/cli/cli/cmd/gh
 	go get -d -v
 	go build -ldflags "-L /system/lib -linkmode external -extldflags \"-lc -static\""
