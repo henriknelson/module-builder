a6bfd2f79f839603f7b89ce4c58adf55e4193f42
Henrik Nelson <henrik@cliffords.nu>
2021-02-09 11:43:40 +0100

    Updated binutils module to 2.36.1

--- modules/binutils/build.sh
+++ modules/binutils/build.sh
@@ -1,27 +1,17 @@
-MAGISK_MODULE_HOMEPAGE=https://www.gnu.org/software/binutils
+MAGISK_MODULE_HOMEPAGE=https://www.gnu.org/software/binutils/
 MAGISK_MODULE_DESCRIPTION="Collection of binary tools, the main ones being ld, the GNU linker, and as, the GNU assembler"
 MAGISK_MODULE_LICENSE="GPL-2.0"
-MAGISK_MODULE_VERSION=2.36
-MAGISK_MODULE_SRCURL=https://ftp.gnu.org/gnu/binutils/binutils-${MAGISK_MODULE_VERSION}.tar.xz
-MAGISK_MODULE_SHA256=5788292cc5bbcca0848545af05986f6b17058b105be59e99ba7d0f9eb5336fb8
-MAGISK_MODULE_DEPENDS="libc++, zlib, libandroid-spawn, libandroid-glob"
+MAGISK_MODULE_MAINTAINER="@termux"
+MAGISK_MODULE_VERSION=2.36.1
+MAGISK_MODULE_SRCURL=https://mirrors.kernel.org/gnu/binutils/binutils-${MAGISK_MODULE_VERSION}.tar.xz
+MAGISK_MODULE_SHA256=e81d9edf373f193af428a0f256674aea62a9d74dfe93f65192d4eae030b0f3b0
+MAGISK_MODULE_DEPENDS="libc++, zlib"
 MAGISK_MODULE_BREAKS="binutils-dev"
 MAGISK_MODULE_REPLACES="binutils-dev"
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="--enable-gold --enable-plugins --disable-werror --with-system-zlib --enable-new-dtags --enable-static --disable-shared"
 MAGISK_MODULE_EXTRA_MAKE_ARGS="tooldir=$MAGISK_PREFIX"
 MAGISK_MODULE_RM_AFTER_INSTALL="share/man/man1/windmc.1 share/man/man1/windres.1 bin/ld.bfd"
 MAGISK_MODULE_NO_STATICSPLIT=true
-MAGISK_MODULE_HAS_DEBUG=false
-# Debug build fails with:
-# ~/termux-build/binutils/src/binutils/readelf.c:19060:81: error: in call to 'fread', size * count is too large for the given buffer
-#     if (fread (ehdr32.e_type, sizeof (ehdr32) - EI_NIDENT, 1, filedata->handle) != 1)
-#                                                                               ^
-# ~/termux-build/_cache/19b-aarch64-24-v5/bin/../sysroot/usr/include/bits/fortify/stdio.h:107:9: note: from 'diagnose_if' attribute on 'fread':
-#        __clang_error_if(__bos(buf) != __BIONIC_FORTIFY_UNKNOWN_SIZE && size * count > __bos(buf),
-#        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ~/termux-build/_cache/19b-aarch64-24-v5/bin/../sysroot/usr/include/sys/cdefs.h:163:52: note: expanded from macro '__clang_error_if'
-# #define __clang_error_if(cond, msg) __attribute__((diagnose_if(cond, msg, "error")))
-#                                                    ^           ~~~~
 
 # Avoid linking against libfl.so from flex if available:
 export LEXLIB=
@@ -34,10 +24,9 @@ magisk_step_pre_configure() {
 	else
 		export LIB_PATH="${MAGISK_PREFIX}/lib:/system/lib64"
 	fi
-	#export CFLAGS+=" -static -O2"
 	export LDFLAGS+=" --static"
-	export LIBS+=" -landroid-glob -landroid-spawn"
-	export CPPFLAGS+=" -I/$MAGISK_PREFIX/include"
+        export LIBS+=" -landroid-glob -landroid-spawn"
+        export CPPFLAGS+=" -I/$MAGISK_PREFIX/include"
 }
 
 magisk_step_post_make_install() {
