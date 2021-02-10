9b3696d31880b8829b57c0203ab6d2b8e2015b21
Henrik Nelson <henrik@cliffords.nu>
2021-02-10 04:13:25 +0100

    Updated krb5 module

--- modules/krb5/build.sh
+++ modules/krb5/build.sh
@@ -1,17 +1,16 @@
 MAGISK_MODULE_HOMEPAGE=https://web.mit.edu/kerberos
 MAGISK_MODULE_DESCRIPTION="The Kerberos network authentication system"
 MAGISK_MODULE_LICENSE="MIT"
-MAGISK_MODULE_VERSION=1.18.2
-MAGISK_MODULE_REVISION=1
-MAGISK_MODULE_SRCURL=https://fossies.org/linux/misc/krb5-1.18.2.tar.gz
-MAGISK_MODULE_SHA256=c6e4c9ec1a98141c3f5d66ddf1a135549050c9fab4e9a4620ee9b22085873ae0
+MAGISK_MODULE_LICENSE_FILE="../NOTICE"
+MAGISK_MODULE_MAINTAINER="@termux"
+MAGISK_MODULE_VERSION=1.19
+MAGISK_MODULE_SRCURL=https://fossies.org/linux/misc/krb5-$MAGISK_MODULE_VERSION.tar.gz
+MAGISK_MODULE_SHA256=bc7862dd1342c04e1c17c984a268d50f29c0a658a59a22bd308ffa007d532a2e
 MAGISK_MODULE_DEPENDS="libandroid-support, libandroid-glob, readline, openssl, libdb"
 MAGISK_MODULE_BREAKS="krb5-dev"
 MAGISK_MODULE_REPLACES="krb5-dev"
-MAGISK_MODULE_MAINTAINER="Vishal Biswas @vishalbiswas"
 MAGISK_MODULE_CONFFILES="etc/krb5.conf var/krb5kdc/kdc.conf"
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
---disable-static
 --with-readline
 --without-system-verto
 --with-netlib=-lc
@@ -19,14 +18,12 @@ MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
 --sbindir=$MAGISK_PREFIX/bin
 --with-size-optimizations
 --with-system-db
---with-system-et
---with-system-ss
 DEFCCNAME=$MAGISK_PREFIX/tmp/krb5cc_%{uid}
 DEFKTNAME=$MAGISK_PREFIX/etc/krb5.keytab
 DEFCKTNAME=$MAGISK_PREFIX/var/krb5/user/%{euid}/client.keytab
 "
 
-magisk_step_post_extract_package() {
+magisk_step_post_extract_module() {
 	MAGISK_MODULE_SRCDIR+="/src"
 }
 
@@ -37,25 +34,31 @@ magisk_step_pre_configure() {
 	export ac_cv_printf_positional='yes'
 
 	# bionic doesn't have getpass
-	cp "$MAGISK_MODULE_BUILDER_DIR/netbsd_getpass.c" "$MAGISK_MODULE_SRCDIR/src/clients/kpasswd/"
+	cp "$MAGISK_MODULE_BUILDER_DIR/netbsd_getpass.c" "$MAGISK_MODULE_SRCDIR/clients/kpasswd/"
 
 	CFLAGS="$CFLAGS -D_PASSWORD_LEN=PASS_MAX"
-	export LIBS="-landroid-glob -llog"
+	export LIBS=" -landroid-glob -ldb -llog"
 }
 
 magisk_step_post_make_install() {
 	# Enable logging to STDERR by default
-	echo -e "\tdefault = STDERR" >> $MAGISK_MODULE_SRCDIR/src/config-files/krb5.conf
+	echo -e "\tdefault = STDERR" >> $MAGISK_MODULE_SRCDIR/config-files/krb5.conf
 
 	# Sample KDC config file
 	install -dm 700 $MAGISK_PREFIX/var/krb5kdc
-	install -pm 600 $MAGISK_MODULE_SRCDIR/src/config-files/kdc.conf $MAGISK_PREFIX/var/krb5kdc/kdc.conf
+	install -pm 600 $MAGISK_MODULE_SRCDIR/config-files/kdc.conf $MAGISK_PREFIX/var/krb5kdc/kdc.conf
 
 	# Default configuration file
-	install -pm 600 $MAGISK_MODULE_SRCDIR/src/config-files/krb5.conf $MAGISK_PREFIX/etc/krb5.conf
+	install -pm 600 $MAGISK_MODULE_SRCDIR/config-files/krb5.conf $MAGISK_PREFIX/etc/krb5.conf
 
 	install -dm 700 $MAGISK_PREFIX/share/aclocal
-	install -m 600 $MAGISK_MODULE_SRCDIR/src/util/ac_check_krb5.m4 $MAGISK_PREFIX/share/aclocal
+	install -m 600 $MAGISK_MODULE_SRCDIR/util/ac_check_krb5.m4 $MAGISK_PREFIX/share/aclocal
+}
 
-	cp -r $MAGISK_MODULE_SRCDIR/src/util/et $MAGISK_PREFIX/include;
+mmagisk_step_pre_configure() {
+	export LDFLAGS="$LDFLAGS -static"
+}
+
+mmagisk_step_configure() {
+	./configure --prefix=$MAGISK_PREFIX --static
 }
--- modules/krb5/netbsd_getpass.c
+++ modules/krb5/netbsd_getpass.c
@@ -28,7 +28,6 @@
  * SUCH DAMAGE.
  */
 #if 0
-#include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
 #if 0
 static char sccsid[] = "@(#)getpass.c	8.1 (Berkeley) 6/4/93";
