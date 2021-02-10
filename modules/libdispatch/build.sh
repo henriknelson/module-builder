34c8dbd7dafe70018f01f59054572ba7006b0e0f
Henrik Nelson <henrik@cliffords.nu>
2021-02-10 04:14:32 +0100

    Updated libdispatch module

--- modules/libdispatch/build.sh
+++ modules/libdispatch/build.sh
@@ -1,10 +1,13 @@
-MAGISK_MODULE_HOMEPAGE=https://github.com/apple/swift-corelibs-libdspatch
+MAGISK_MODULE_HOMEPAGE=https://github.com/apple/swift-corelibs-libdispatch
 MAGISK_MODULE_DESCRIPTION="The libdispatch project, for concurrency on multicore hardware"
 MAGISK_MODULE_LICENSE="Apache-2.0"
-_VERSION=5.3
+MAGISK_MODULE_MAINTAINER="@buttaface"
+_VERSION=5.3.3
 MAGISK_MODULE_VERSION=1:${_VERSION}
-MAGISK_MODULE_REVISION=1
 MAGISK_MODULE_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${_VERSION}-RELEASE.tar.gz
-MAGISK_MODULE_SHA256=6805b555aab65d740fccaa99570fd29b32efa6c310fd42524913e44509dc4969
+MAGISK_MODULE_SHA256=84a482afefdcda26c7dc83e3b75e662ed7705786a34a6b4958c0cdc6cace2c46
 MAGISK_MODULE_DEPENDS="libc++, libblocksruntime"
-#MAGISK_MODULE_EXTRA_CONFIGURE_ARGS+=" --disable-shared"
+
+magisk_module_pre_configure() {
+	export LDFLAGS+=" -static"
+}
--- modules/libdispatch/libblocksruntime.submodule.sh
+++ modules/libdispatch/libblocksruntime.submodule.sh
@@ -1,2 +1,2 @@
-MAGISK_SUBMODULE_INCLUDE="include/Block.h lib/libBlocksRuntime.so lib/libBlocksRuntime.a"
+MAGISK_SUBMODULE_INCLUDE="include/Block.h lib/libBlocksRuntime.so"
 MAGISK_SUBMODULE_DESCRIPTION="LLVM Blocks runtime library"
