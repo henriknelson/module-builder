9dfbba7d5a81bda4f8171505f7caf4b2411db3ba
Henrik Nelson <henrik@cliffords.nu>
2021-02-12 20:35:34 +0100

    Updated strace module to 5.10

--- modules/strace/build.sh
+++ modules/strace/build.sh
@@ -1,18 +1,23 @@
 MAGISK_MODULE_HOMEPAGE=https://strace.io/
 MAGISK_MODULE_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 MAGISK_MODULE_LICENSE="BSD"
-MAGISK_MODULE_VERSION=5.9
+MAGISK_MODULE_MAINTAINER="@termux"
+MAGISK_MODULE_VERSION=5.10
 MAGISK_MODULE_SRCURL=https://github.com/strace/strace/releases/download/v$MAGISK_MODULE_VERSION/strace-$MAGISK_MODULE_VERSION.tar.xz
-MAGISK_MODULE_SHA256=39473eb8465546c3e940fb663cb381eba5613160c7302794699d194a4d5d66d9
+MAGISK_MODULE_SHA256=fe3982ea4cd9aeb3b4ba35f6279f0b577a37175d3282be24b9a5537b56b8f01c
 #MAGISK_MODULE_DEPENDS="libdw"
-MAGISK_MODULE_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
+
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
 MAGISK_MODULE_EXTRA_CONFIGURE_ARGS="
 st_cv_m32_mpers=no
 --enable-mpers=no
 "
 
+# This is a perl script.
+MAGISK_MODULE_RM_AFTER_INSTALL="bin/strace-graph"
+
 magisk_step_pre_configure() {
+	#CFLAGS+=" -I $MAGISK_PREFIX/include/elfutils -O2 -static"
 	CPPFLAGS+=" -Dfputs_unlocked=fputs"
 	LDFLAGS+=" -static -pthread"
 }
